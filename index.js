d2f57d86ababb8e61af58067613005260188f54e
c-nakashima <nakashima@penqe.com>
2021-02-25 01:43:20 +0900

    add animation

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 // import anime from 'anime.min.js';
+// const anime = require('animejs');
 
-// anime({
-//   targets: '#elem',
-//   translateX: 250,
-//   rotate: '1turn',
-//   backgroundColor: '#384878',
-//   duration: 800
-// });
+anime({
+  targets: '.about',
+  translateX: -1000,
+  duration: 2000,
+  easing: 'easeOutQuart',
+});
+
+anime({
+  targets: '.works',
+  translateX: 1000,
+  duration: 2000,
+  easing: 'easeOutQuart',
+});
+
+anime({
+  targets: '.contact',
+  translateX: -1000,
+  duration: 2000,
+  easing: 'easeOutQuart',
+});
+
 
 $(function(){
   $(".service").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
+
+$(function(){
+  $(".work").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
+
+$(function(){
+  $(".work-img").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
+
+$(function(){
+  $(".work-description").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
+
+$(function(){
+  $("h2").on("inview", function (event, isInView) {
     if (isInView) {
       $(this).stop().addClass("is-show");
     }
