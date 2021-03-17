d07d450f5d132bf6c38f8d651fd074c7dac686d8
c-nakashima <nakashima@penqe.com>
2021-03-18 01:35:43 +0900

    add onload function

--- JS/index.js
+++ JS/index.js
@@ -1,111 +1,96 @@
-document.addEventListener('DOMContentLoaded', function(){
-  let trigger = new ScrollTrigger.default();
-  trigger.add('.about', {
-    toggle: {
-      callback: {
-        in: () => {
-          text_conv();
-          // anime.jsのコード
-          anime({
-            targets: '.about',
-            translateX: -1000,
-            duration: 2000,
-            easing: 'easeOutQuart',
-          });
-          // anime.js ここまで
-        },
-        out: () => {
-        }
-      }
-    }
-  });
-});
 
-//slidein
-// anime({
-//   targets: '.about',
-//   translateX: -1000,
-//   duration: 2000,
-//   easing: 'easeOutQuart',
+//▼ text_conv is not definedとなってしまい動作しませんでした
+// document.addEventListener('DOMContentLoaded', function(){
+//   let trigger = new ScrollTrigger.default();
+//   trigger.add('.about', {
+//     toggle: {
+//       callback: {
+//         in: () => {
+//           text_conv();
+//           // anime.jsのコード
+//           anime({
+//             targets: '.about',
+//             translateX: -1000,
+//             duration: 2000,
+//             easing: 'easeOutQuart',
+//           });
+//           // anime.js ここまで
+//         },
+//         out: () => {
+//         }
+//       }
+//     }
+//   });
 // });
 
-anime({
-  targets: '.works',
-  translateX: 1000,
-  duration: 2000,
-  easing: 'easeOutQuart',
-});
 
-anime({
-  targets: '.contact',
-  translateX: -1000,
-  duration: 2000,
-  easing: 'easeOutQuart',
-});
-
-//fade in
-$(function(){
-  $(".service").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
-    }
+//onload
+window.onload = function(){
+  $(".heading-top").stop().addClass("is-show");
+  anime({
+    targets: '.about',
+    translateX: -1000,
+    duration: 2000,
+    easing: 'easeOutQuart',
   });
-});
+}
 
-$(function(){
-  $(".work").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
+//onscroll
+window.onscroll = function()
+{
+  let getTargetOffset = function(target){
+    const targetOffset = target.getBoundingClientRect();
+    return targetOffset.top;
+  }
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  let bottom = scrollTop + document.documentElement.clientHeight;
+  let showContent = function(target, heading, fadeInContent, slideInContent,translateX){
+    let top = getTargetOffset(target);
+    if( top < bottom){
+      //fade in
+      $(heading).stop().addClass("is-show");
+      $(fadeInContent).on("inview", function (isInView) {
+        if (isInView) {
+          $(this).stop().addClass("is-show");
+        }
+      });
+      //slide in
+      anime({
+        targets: slideInContent,
+        translateX: translateX,
+        duration: 2000,
+        easing: 'easeOutQuart',
+      });
     }
-  });
-});
+  }
 
-$(function(){
-  $(".work-img").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
-    }
-  });
-});
-
-$(function(){
-  $(".work-description").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
-    }
-  });
-});
-
-$(function(){
-  $("h2").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
-    }
-  });
-});
+  showContent(about,".heading-top",".service",".about",-1000);
+  showContent(works,".heading-below",".work",".works",1000);
+  showContent(contact,".heading-below",".contact",".contact",-1000);
+}
 
 //move to target position
 $(function(){
 $('.scroll-btn').click(function() {
-let scrollBtn= $(this).attr("id");
-let target;
-switch (scrollBtn){
-  case 'aboutBtn':
-    target = $('#about');
-    break;
-  case 'worksBtn':
-    target = $('#works');
-    break;
-  case 'contactBtn':
-    target = $('#contact');
-    break;
-  case 'arrowBtn':
-    target = $('html');
-    break;
-}
-let position = target.offset().top;
-$('body,html').animate({scrollTop:position}, 800, 'swing');
-return false;
-});
+  let scrollBtn= $(this).attr("id");
+  let target;
+  switch (scrollBtn){
+    case 'aboutBtn':
+      target = $('#about');
+      break;
+    case 'worksBtn':
+      target = $('#works');
+      break;
+    case 'contactBtn':
+      target = $('#contact');
+      break;
+    case 'arrowBtn':
+      target = $('html');
+      break;
+  }
+  let position = target.offset().top;
+  $('body,html').animate({scrollTop:position}, 800, 'swing');
+  return false;
+  });
 });
 
