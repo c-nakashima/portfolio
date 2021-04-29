cfc44d91e5f380a84503776a16f5f252a3c33fba
Chisato <chisato@Chisatos-Mac-Book-Pro.local>
2021-04-30 03:14:03 +0900

    add scroll animation

--- JS/test.js
+++ JS/test.js
@@ -1,5 +1,36 @@
 $(function () {
-  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
-    $('.menu , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  $('.js-btn').on('click', function () { 
+    $('.menu , .btn-line').toggleClass('open');
   })
 });
+
+//move to target position
+const windowWidth = $(window).width();
+$(function(){
+  $('.scroll-btn').click(function() {
+    let scrollBtn= $(this).attr("id");
+    let target;
+    switch (scrollBtn){
+      case 'aboutBtn':
+        target = $('#about');
+        break;
+      case 'worksBtn':
+        target = $('#works');
+        break;
+      case 'contactBtn':
+        target = $('#contact');
+        break;
+      case 'arrowBtn':
+        target = $('html');
+        break;
+    }
+    let position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, 800, 'swing');
+    if(windowWidth < 600){
+      $(function(){
+        $('.menu , .btn-line').toggleClass('open');
+      })
+    }
+    return false;
+    });
+  });
