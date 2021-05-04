094321e255d0cc957f75ec96eee0d3591db7a589
Chisato <chisato@Chisatos-Mac-Book-Pro.local>
2021-05-04 15:45:56 +0900

    ass portforio

--- JS/test.js
+++ JS/test.js
@@ -5,7 +5,6 @@ $(function () {
 });
 
 //move to target position
-const windowWidth = $(window).width();
 $(function(){
   $('.scroll-btn').click(function() {
     let scrollBtn= $(this).attr("id");
@@ -26,7 +25,7 @@ $(function(){
     }
     let position = target.offset().top;
     $('body,html').animate({scrollTop:position}, 800, 'swing');
-    if(windowWidth < 600){
+    if($(window).width() < 600){
       $(function(){
         $('.menu , .btn-line').toggleClass('open');
       })
@@ -34,3 +33,41 @@ $(function(){
     return false;
     });
   });
+
+//onscroll
+//onscroll
+window.onscroll = function()
+{
+  let getTargetOffset = function(target){
+    const targetOffset = target.getBoundingClientRect();
+    return targetOffset.top;
+  }
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  let bottom = scrollTop + document.documentElement.clientHeight;
+  // let showContent = function(target, heading, fadeInContent){
+  //   let top = getTargetOffset(target);
+  //   if( top < bottom){
+  //     //fade in
+  //     $(heading).stop().addClass("-showing");
+  //     $(fadeInContent).on("inview", function (isInView) {
+  //       if (isInView) {
+  //         $(this).stop().addClass("-showing");
+  //       }
+  //     });
+  //   }
+  // }
+
+  // showContent(about,".heading-top",".service",".about");
+  // showContent(works,".heading-below",".work",".works");
+  // showContent(contact,".heading-below",".contact",".contact");
+
+  let top = getTargetOffset(works);
+  if( top < bottom){
+    console.log('hoge')
+    //fade in
+    $(".container").stop().addClass("-showing");
+    $(".work-description").stop().addClass("-showing");
+  }
+
+  
+}
